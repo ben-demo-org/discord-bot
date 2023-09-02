ec1bbfe79752d8e5ea6aa28f653e1e30a93042fe
Ben Ngo <benpngo88@gmail.com>
2023-09-02 10:06:32 -0500

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,27 @@
-# Enables the Cloud Run API
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
+## Enables the Cloud Run API
+#resource "google_project_service" "run_api" {
+#  service = "run.googleapis.com"#
 
-  disable_on_destroy = true
-}
+#  disable_on_destroy = true
+#}
+## Create the Cloud Run service
+#resource "google_cloud_run_service" "run_service" {
+#  name = "app"#
+#
 
-# Create the Cloud Run service
-resource "google_cloud_run_service" "run_service" {
-  name = "app"
+#  template {
+#    spec {
+#      containers {
+#        image = "gcr.io/google-samples/hello-app:1.0"
+#      }
+#    }
+#  }#
 
+#  traffic {
+#    percent         = 100
+#    latest_revision = true
+#  }#
 
-  template {
-    spec {
-      containers {
-        image = "gcr.io/google-samples/hello-app:1.0"
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
-  # Waits for the Cloud Run API to be enabled
-  depends_on = [google_project_service.run_api]
-}
+#  # Waits for the Cloud Run API to be enabled
+#  depends_on = [google_project_service.run_api]
+#}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,9 +4,9 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
-    google           = "4.80.0"
+    #google           = "4.80.0"
     required_version = "1.5.5"
   }
 }
 
-provider "google" {}
+#provider "google" {}

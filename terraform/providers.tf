05d16486dedb82316b89c6054a45ac25712aaca7
Ben Ngo <benpngo88@gmail.com>
2023-09-02 09:45:45 -0500

    Push initial

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,15 +4,14 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    google = {
+      source = "hashicorp/google"
+      version = "4.80.0"
     }
     required_version = ">= 1.5.0"
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
+# Configure the Google Provider
+provider "google" {
 }

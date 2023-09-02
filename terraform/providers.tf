f059a60e4e34260c1e6bffd723f9b30b74cb5ace
Ben Ngo <benpngo88@gmail.com>
2023-09-02 10:05:32 -0500

    adad

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,7 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "4.80.0"
-    }
+    google           = "4.80.0"
     required_version = "1.5.5"
   }
 }
